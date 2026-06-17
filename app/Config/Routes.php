1fdd37cbd50a349b3441bf45e0aa3292272f3e24
nofyrabe777 <nofyrabe@gmail.com>
2026-06-17 09:26:08 +0300

    Diovina koa Nofy

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,4 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/produit','Produit::index');
-$routes->get('/produit/(:num)', 'Produit::show/$1');
+
